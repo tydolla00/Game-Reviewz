26e2398bb6ced681e7b137a0313d567726f4449b
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2023-03-15 23:47:04 -0400

    Added Login Functionality

--- src/frontend/src/components/login.js
+++ src/frontend/src/components/login.js
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { ArrowClockwise } from "react-bootstrap-icons";
+import AuthService from "../services/AuthService";
+
 const LoginForm = ({
   username,
   handleChange,
@@ -5,16 +10,35 @@ const LoginForm = ({
   handlePassChange,
   setLogin,
 }) => {
+  const [loading, setLoading] = useState(false);
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log(data);
+    console.log(JSON.stringify(data, null, 2));
+    AuthService.login(data.email, data.password)
+      .then((response) => {
+        console.log(response);
+        alert("Successfully logged in");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="formGroup">
         <label>Username:</label>
         <input
           type="text"
+          name="username"
           className="form-control"
           placeholder="Enter your username"
           onChange={handleChange}
-          value={username}
+          {...register("email")}
+          //   value={username}
           required
         />
       </div>
@@ -22,14 +46,19 @@ const LoginForm = ({
         <label>Password:</label>
         <input
           type="password"
+          name="password"
           className="form-control"
           placeholder="Enter your password"
           onChange={handlePassChange}
-          value={password}
+          {...register("password")}
+          //   value={password}
           required
         />
       </div>
-      <button className="btn"> Login</button>
+      <button className="btn" type="submit">
+        <span>{loading && <ArrowClockwise className="loading" />}</span>
+        Login
+      </button>
       <button className="btn signup" onClick={() => setLogin(false)}>
         Sign Up
       </button>
