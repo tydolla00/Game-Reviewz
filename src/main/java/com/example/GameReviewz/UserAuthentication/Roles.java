619511fdde9d55f98ac280356076bd0887c5adb0
tydolla00 <90355178+tydolla00@users.noreply.github.com>
2023-04-10 14:01:06 -0400

    Resolving Merge Conflicts

--- src/main/java/com/example/GameReviewz/UserAuthentication/Roles.java
+++ src/main/java/com/example/GameReviewz/UserAuthentication/Roles.java
@@ -1,2 +1,21 @@
-package com.example.GameReviewz.UserAuthentication;public class Roles {
+package com.example.GameReviewz.UserAuthentication;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private Role name;
+
 }
