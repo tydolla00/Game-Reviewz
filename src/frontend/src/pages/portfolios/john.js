3e61d8ac00cc61f4b12fe9158b0403751ec8454a
JohnBeltran4 <johnbeltran4561@gmail.com>
2023-04-17 19:41:50 -0400

    fixed navbar and eddited profile page

--- src/frontend/src/pages/portfolios/john.js
+++ src/frontend/src/pages/portfolios/john.js
@@ -1,1 +1,26 @@
-export default function JohnPage() {}
+import "../portfolios/John.scss";
+
+export default function JohnPage() {
+    return (
+        <div>
+            <div className={"container"}>
+                <div className={"profileContainer"}>
+                    <div className={"profileIntro"}>
+                        <h1 className={"greeting"}>
+                            Hey there!
+                        </h1>
+                        <p className={"greetingPara"}>
+                            I'm John and I am a front end developer who is proficient in Java, React, HTML, and CSS.
+                            I am a recent graduate with a Bachelor's degree in Computer Science, and not only that I have
+                            have earned multiple certifications from LinkedIn, and freeCodeCamp.org.
+                            Even still, there is still so much to learn so I hope to improve my skills even more to create the best results possible.
+                        </p>
+                    </div>
+                    <div className={"profilePhoto"}>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
